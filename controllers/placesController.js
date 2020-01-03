770d95090ffad11f9614490cc6c9c94412caa6b5
Abdulla Bashiroglu <abdullabashir32@gmail.com>
2020-01-03 23:17:31 +0100

    added the same funct. with controllers

---
+++ controllers/placesController.js
@@ -0,0 +1,40 @@
+const GlobalError = require('../models/GlobalError');
+
+const PLACES_STATIC_ARRAY = [
+  {
+    id: 'p1',
+    title: 'Empire State Building',
+    description: 'One of the most famous sky scrapers in the world!',
+    location: {
+      lat: 40.7484474,
+      lng: -73.9871516
+    },
+    address: '20 W 34th St, New York, NY 10001',
+    creator: 'u1'
+  }
+];
+
+const getPlacebyPlaceId = (req, res, next) => {
+  const placeId = req.params.placeId;
+  const place = PLACES_STATIC_ARRAY.find(p => p.id === placeId);
+  if (!place) {
+    throw new GlobalError('Could not find a place for the provided id.', 404);
+  }
+  res.json({ place });
+};
+
+const getPlacebyUserId = (req, res, next) => {
+  const userId = req.params.userId;
+  const place = PLACES_STATIC_ARRAY.find(p => p.creator === userId);
+
+  if (!place) {
+    return next(
+      new GlobalError('Could not find a place for the provided user id.', 404)
+    );
+  }
+
+  res.json({ place });
+};
+
+exports.getPlacebyUserId = getPlacebyUserId;
+exports.getPlacebyPlaceId = getPlacebyPlaceId;
--- routes/placesRoutes.js
+++ routes/placesRoutes.js
@@ -1,43 +1,11 @@
 const express = require('express');
 
-const GlobalError = require('../models/GlobalError');
+const placesController = require('../controllers/placesController');
 
 const router = express.Router();
 
-const PLACES_STATIC_ARRAY = [
-  {
-    id: 'p1',
-    title: 'Empire State Building',
-    description: 'One of the most famous sky scrapers in the world!',
-    location: {
-      lat: 40.7484474,
-      lng: -73.9871516
-    },
-    address: '20 W 34th St, New York, NY 10001',
-    creator: 'u1'
-  }
-];
+router.get('/:placeId', placesController.getPlacebyPlaceId);
 
-router.get('/:placeId', (req, res, next) => {
-  const placeId = req.params.placeId;
-  const place = PLACES_STATIC_ARRAY.find(p => p.id === placeId);
-  if (!place) {
-    throw new GlobalError('Could not find a place for the provided id.', 404);
-  }
-  res.json({ place });
-});
-
-router.get('/user/:userId', (req, res, next) => {
-  const userId = req.params.userId;
-  const place = PLACES_STATIC_ARRAY.find(p => p.creator === userId);
-
-  if (!place) {
-    return next(
-      new GlobalError('Could not find a place for the provided user id.', 404)
-    );
-  }
-
-  res.json({ place });
-});
+router.get('/user/:userId', placesController.getPlacebyUserId);
 
 module.exports = router;
