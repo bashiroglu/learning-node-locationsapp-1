bf9115b88873ef20bce338be2ccfa2b6e8aa51ab
Abdulla Bashiroglu <abdullabashir32@gmail.com>
2020-01-04 22:40:24 +0100

    added user modal and funct. by using db

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,62 +1,95 @@
 const uuid = require('uuid/v4');
 const GlobalError = require('../models/GlobalError');
 const { validationResult } = require('express-validator');
+const User = require('../models/user');
 
-const USERS_STATIC_ARRAY = [
-  {
-    id: 'u1',
-    name: 'Abdulla Bahsir',
-    email: 'abdulla@locationapp.com',
-    password: 'test1234'
+const getUsers = async (req, res, next) => {
+  let users;
+  try {
+    users = await User.find({}, '-password');
+  } catch (err) {
+    const error = new GlobalError(
+      'Fetching users failed, please try again later.',
+      500
+    );
+    return next(error);
   }
-];
-
-const getUsers = (req, res, next) => {
-  res.json({ users: USERS_STATIC_ARRAY });
+  res.json({users: users.map(user => user.toObject({ getters: true }))});
 };
 
-const signup = (req, res, next) => {
+const signup = async (req, res, next) => {
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
-    throw new GlobalError(
-      'Invalid inputs passed, please check your data.',
+    return next(
+      new GlobalError('Invalid inputs passed, please check your data.', 422)
+    );
+  }
+  const { name, email, password, places } = req.body;
+
+  let existingUser
+  try {
+    existingUser = await User.findOne({ email })
+  } catch (err) {
+    const error = new GlobalError(
+      'Signing up failed, please try again later.',
+      500
+    );
+    return next(error);
+  }
+  
+  if (existingUser) {
+    const error = new GlobalError(
+      'User exists already, please login instead.',
       422
     );
-  } /* this f. will detect we have validation error from routes or not */
-  const { name, email, password } = req.body;
-
-  const hasUser = USERS_STATIC_ARRAY.find(u => u.email === email);
-  if (hasUser) {
-    throw new GlobalError(
-      'Could not create user, email already exists.',
-      422 /* invalid user input code*/
-    );
+    return next(error);
   }
-
-  const createdUser = {
-    id: uuid(),
+  
+  const createdUser = new User({
     name,
     email,
-    password
-  };
+    image: 'https://live.staticflickr.com/7631/26849088292_36fc52ee90_b.jpg',
+    password,
+    places
+  });
 
-  USERS_STATIC_ARRAY.push(createdUser);
-
-  res.status(201).json({ user: createdUser });
-};
-
-const login = (req, res, next) => {
-  const { email, password } = req.body;
-
-  const userInDb = USERS_STATIC_ARRAY.find(u => u.email === email);
-  if (!userInDb || userInDb.password !== password) {
-    throw new GlobalError(
-      'Could not identify user, credentials seem to be wrong.',
-      401
+  try {
+    await createdUser.save();
+  } catch (err) {
+    const error = new GlobalError(
+      'Signing up failed, please try again.',
+      500
     );
+    return next(error);
   }
 
-  res.json({ message: 'Logged in!' });
+  res.status(201).json({user: createdUser.toObject({ getters: true })});
+};
+
+const login = async (req, res, next) => {
+  const { email, password } = req.body;
+
+  let existingUser;
+
+  try {
+    existingUser = await User.findOne({ email })
+  } catch (err) {
+    const error = new GlobalError(
+      'Logging in failed, please try again later.',
+      500
+    );
+    return next(error);
+  }
+
+  if (!existingUser || existingUser.password !== password) {
+    const error = new GlobalError(
+      'Invalid credentials, could not log you in.',
+      401
+    );
+    return next(error);
+  }
+
+  res.json({message: 'Logged in!'});
 };
 
 exports.getUsers = getUsers;
